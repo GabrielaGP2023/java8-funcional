f4c3f82824ac49e16be01cc931573d5ac5e99d20
GabrielaGP2023 <cursos@gabrielagomezpuente.es>
2024-08-02 10:31:19 +0200

    proyecto fundamentos programación funcional

---
+++ src/main/java/basic/Product.java
@@ -0,0 +1,18 @@
+package basic;
+
+
+import lombok.*;
+
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Data
+public class Product {
+
+    private int id;
+    private String name;
+
+
+}
